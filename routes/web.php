a00d1c07b2ef81de2fa0f87d35c659c6c49f4a91
Jeff Githiri <geoffrey.githiri@andela.com>
2019-10-22 17:47:21 +0300

    27 spas and the backend. creates and fetches statuses from the backend using axios

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,17 @@
 |
 */
 
+use App\Status;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-
 Route::get('projects/create', 'ProjectController@create');
 Route::post('projects', 'ProjectController@store');
+
+
+Route::get('statuses', function () {
+    return Status::with('user')->latest()->get();
+});
