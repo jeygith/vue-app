788baf7cd1695c28add495b02d0754ecc29f4bb6
Jeff Githiri <geoffrey.githiri@andela.com>
2019-10-22 13:46:32 +0300

    create project.create and list files

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('projects/create', 'ProjectController@create');
+Route::post('projects', 'ProjectController@store');
